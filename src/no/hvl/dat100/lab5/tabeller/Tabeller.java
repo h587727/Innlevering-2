846d8d915702a8ed4dbcecd7165b467fcd01c9b5
h587727 <587727@stud.hvl.no>
2020-09-27 18:21:46 +0200

    Oblig2

---
+++ src/no/hvl/dat100/lab5/tabeller/Tabeller.java
@@ -0,0 +1,165 @@
+package no.hvl.dat100.lab5.tabeller;
+
+import java.util.Arrays;
+
+public class Tabeller {
+
+	// a)
+	public static void skrivUt(int[] tabell) {
+
+		// TODO
+		//throw new UnsupportedOperationException("skrivUt ikke implementert");
+
+		
+		System.out.print("[ ");
+
+		for (int tall : tabell) {
+			System.out.print(tall + " ");
+		}
+
+		System.out.println("]");
+
+}
+	
+
+	// b)
+	public static String tilStreng(int[] tabell) {
+	//	throw new UnsupportedOperationException("tilStreng ikke implementert");
+		String number = "[";
+		
+		for (int i = 0; i < tabell.length; i++) {
+			if(i < tabell.length-1) {
+				number += tabell[i] + ",";
+			}else
+				number += tabell[i];
+			
+		}
+		
+		number += "]";
+		return number;
+		
+	}
+			
+
+
+
+	// c)
+	public static int summer(int[] tabell) {
+
+		// for løkke
+		int sum = 0;
+		for(int i = 0; i < tabell.length; i++) {
+			sum += tabell[i];
+			
+		}
+		System.out.println(sum + ", ");
+		return sum;
+	}
+	
+		
+	// while løkke
+		
+//		int sum = 0;
+//		int i = 0;
+//		while (i < tabell.length) {
+//			sum += tabell[i];
+//			i++;
+//		}
+//		System.out.println(sum);
+//		return sum;
+//		
+//		
+//		
+//		
+//	}
+	
+	
+	
+		
+		
+//	utvidet for løkke	
+	
+//		int sum = 0;
+//		for (int tall:tabell)
+//			sum += tall;
+//		
+//		return sum;
+//
+//	}
+	
+	
+	
+	
+	
+
+	// d)
+	public static boolean finnesTall(int[] tabell, int tall) {
+
+		boolean funnet = false;
+		int pos = 0;
+		while(!funnet && pos < tabell.length) {
+			if(tall == tabell[pos]) {
+				funnet = true;
+			}
+			
+			pos++;
+		}
+		return funnet;
+	}
+
+	// e)
+	public static int posisjonTall(int[] tabell, int tall) {
+
+		int index = -1;
+		
+		for (int i = 0; i < tabell.length && index == -1; i ++)
+			if (tabell[i] == tall)
+				index = i;
+		
+		return index;
+		
+	}
+
+	// f)
+	public static int[] reverser(int[] tabell) {
+
+	int[] nyTabell = new int[tabell.length];
+	int  telling = tabell.length-1;
+	
+	for (int i = 0; i < tabell.length; i++) {
+		nyTabell[i] = tabell[telling];
+		telling--;
+	}
+	
+	return nyTabell;
+	
+	}
+
+	// g)
+	public static boolean erSortert(int[] tabell) {
+		boolean sortert = true;
+		for (int i = 0; i < tabell.length -1 && sortert; i++)
+			if (tabell[i] > tabell[i + 1])
+				sortert = false;
+		
+		return sortert;
+	}
+
+	// h)
+	public static int[] settSammen(int[] tabell1, int[] tabell2) {
+
+		int[] sammen = new int[tabell1.length + tabell2.length];
+		
+		
+		for (int i = 0; i < tabell1.length; i++) {
+			sammen[i] = tabell1[i];
+			
+		}
+		for (int j=0; j<tabell2.length; j++) {
+			sammen[tabell1.length +j] = tabell2[j];
+			
+		}
+		
+		return sammen;
+	}
+}
---
+++ src/no/hvl/dat100/lab6/matriser/Matriser.java
@@ -0,0 +1,81 @@
+package no.hvl.dat100.lab6.matriser;
+
+public class Matriser {
+
+	// a)
+	public static void skrivUt(int[][] matrise) {
+		
+	for(int[] rad : matrise) {
+		for(int i : rad) {
+			System.out.print(i + ",");
+		}
+		System.out.println("");
+	}
+}
+
+	// b)
+	public static String tilStreng(int[][] matrise) {
+
+		String Streng = "";
+		for(int [] rad : matrise) {
+			for(int i : rad) {
+				Streng += i + " ";
+			}
+			Streng +="\n";
+			
+		}
+		return Streng;
+	}
+
+	// c)
+	public static int[][] skaler(int tall, int[][] matrise) {
+		
+	int [][] matriseA = new int[matrise.length][matrise[0].length];
+	
+	for (int rad = 0; rad < matrise.length; rad++) {
+		for(int i = 0; i < matrise[rad].length; i++) {
+			matriseA[rad][i] = matrise[rad][i]*tall;
+		}
+	}
+	return matriseA;
+}
+
+	// d)
+	public static boolean erLik(int[][] a, int[][] b) {
+
+	boolean erLike = true;
+	
+	if (a.length != b.length)
+		erLike = false;
+	
+	if (a.length > 0 && b.length > 0 && erLike)
+		if (a[0].length != b[0].length)
+			erLike = false;
+	
+	for (int y = 0; y < a.length && erLike; y++)
+		for (int x = 0; x < a[y].length && erLike; x++)
+			if (a[y][x] != b[y][x])
+				erLike = false;
+	
+	
+	return erLike;
+}
+
+	
+	// e)
+	public static int[][] speile(int[][] matrise) {
+
+	int[][] nyMatrise = new int[matrise[0].length][matrise.length];
+	
+	for (int y = 0; y < matrise.length; y++)
+		for (int x = 0; x < matrise[y].length; x++)
+			nyMatrise[x][y] = matrise[y][x];
+	
+	return nyMatrise;
+	}
+
+	// f)
+	// fikk ikke til denne 
+	
+	
+}
